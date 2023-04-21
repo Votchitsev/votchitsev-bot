945d5a3920263fa14fbe61bbb534b4d22a372a48
Dmitry <dmitry.votchitsev@gmail.com>
2023-04-21 23:21:15 +0200

    Добавил меню, но не подключил обработчики

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 import asyncio
+from aiogram.types import BotCommand
 
 import config
 import handlers
@@ -21,6 +22,14 @@ async def main():
     dp.include_router(currency_exchange_handlers.router)
     dp.include_router(animal_handlers.router)
     dp.include_router(polls_handlers.router)
+
+    await bot.set_my_commands([
+        BotCommand(command='start', description='Начать'),
+        BotCommand(command='weather', description='Узнать погоду в городе'),
+        BotCommand(command='currency', description='Узнать курс валют'),
+        BotCommand(command='animal', description='Картинка смешного животного'),
+        BotCommand(command='polls', description='Создать опрос и отправить в группу')
+        ])
 
     await dp.start_polling(bot, skip_updates=True)
 
